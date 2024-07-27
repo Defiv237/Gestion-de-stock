b6e94b530c05cc95215516125099fa44d2353e2f
stacvirus <parfaitandre@gmail.com>
2024-07-27 07:25:37 +0100

    debugging the create tables and insertion queries done

--- gestion_stockBD.sql
+++ gestion_stockBD.sql
@@ -5,88 +5,79 @@ CREATE DATABASE Gestion_stock CHARACTER SET 'utf8';
 
 USE Gestion_stock;
 
-DROP TABLE IF EXISTS Commandes;
-
-CREATE TABLE Commandes (
-    id_commande VARCHAR(15) NOT NULL,
-    quantite INT(5) NOT NULL,
-    date_commande DATE NOT NULL,
-    id_produit VARCHAR(15),
-    PRIMARY KEY (id_commande)
-) ENGINE = InnoDB;
-
 DROP TABLE IF EXISTS Categories;
 
 CREATE TABLE Categories (
-    id_categorie VARCHAR(15) NOT NULL,
+    id_categorie int(50) NOT NULL auto_increment,
     nom_categorie VARCHAR(20) NOT NULL,
     PRIMARY KEY (id_categorie)
-) ENGINE = InnoDB;
-
-DROP TABLE IF EXISTS Produits;
-
-CREATE TABLE Produits (
-    id_produit VARCHAR(15) NOT NULL,
-    nom_produit VARCHAR(20) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    prix REAL(10) NOT NULL,
-    stock INT(5) NOT NULL,
-    id_categorie VARCHAR(15),
-    id_fournisseur VARCHAR(15),
-    PRIMARY KEY (id_produit)
 ) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS Fournisseurs;
 
 CREATE TABLE Fournisseurs (
-    id_fournisseur VARCHAR(15) NOT NULL,
-    nom_fournisseur VARCHAR(20) NOT NULL,
-    contact VARCHAR(15) NOT NULL,
-    adresse VARCHAR(20) NOT NULL,
+    id_fournisseur int(50) NOT NULL auto_increment,
+    nom_fournisseur VARCHAR(50) NOT NULL,
+    contact VARCHAR(50) NOT NULL,
+    adresse VARCHAR(50) NOT NULL,
     telephone VARCHAR(15) NOT NULL,
     PRIMARY KEY (id_fournisseur)
 ) ENGINE = InnoDB;
 
-ALTER TABLE Commandes
-ADD CONSTRAINT FK_Commandes_id_produit FOREIGN KEY (id_produit) REFERENCES Produits (id_produit);
+DROP TABLE IF EXISTS Produits;
 
-ALTER TABLE Produits
-ADD CONSTRAINT FK_Produits_id_categorie FOREIGN KEY (id_categorie) REFERENCES Categories (id_categorie);
+CREATE TABLE Produits (
+    id_produit int(50) auto_increment NOT NULL,
+    nom_produit VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    prix REAL NOT NULL,
+    stock INT(5) NOT NULL,
+    id_categorie int not null,
+    id_fournisseur int not null,
+    PRIMARY KEY (id_produit),
+    foreign key (id_categorie) references Categories(id_categorie),
+    foreign key (id_fournisseur) references Fournisseurs(id_fournisseur)
+) ENGINE = InnoDB;
 
-ALTER TABLE Produits
-ADD CONSTRAINT FK_Produits_id_fournisseur FOREIGN KEY (id_fournisseur) REFERENCES Fournisseurs (id_fournisseur);
+DROP TABLE IF EXISTS Commandes;
+
+CREATE TABLE Commandes (
+    id_commande int (50) auto_increment NOT NULL,
+    quantite INT (5) NOT NULL,
+    date_commande DATE NOT NULL,
+    id_produit int not null,
+    PRIMARY KEY (id_commande),
+    foreign key (id_produit) references Produits(id_produit)
+) ENGINE = InnoDB;
+
 
 #insertion des données
 INSERT INTO
-    Categories (id_categorie, nom_categorie)
-VALUES ('C1', 'Électronique'),
-    ('C2', 'Livres'),
-    ('C3', 'Vêtements');
+    Categories (nom_categorie)
+VALUES ('Électronique'),
+    ('Livres'),
+    ('Vêtements');
 
 INSERT INTO
     Fournisseurs (
-        id_fournisseur,
-        nom,
+        nom_fournisseur,
         contact,
         adresse,
         telephone
     )
 VALUES (
-        'F1',
         'Fournisseur A',
         'contactA@example.com',
         '123 Rue Exemple',
         '0123456789'
     ),
     (
-        'F2',
         'Fournisseur B',
         'contactB@example.com',
         '456 Rue Exemple',
         '0123456790'
     ),
     (
-        'F3',
         'Fournisseur C',
         'contactC@example.com',
         '789 Rue Exemple',
@@ -95,8 +86,7 @@ VALUES (
 
 INSERT INTO
     Produits (
-        id_produit,
-        nom,
+        nom_produit,
         description,
         prix,
         stock,
@@ -104,34 +94,38 @@ INSERT INTO
         id_fournisseur
     )
 VALUES (
-        'P4',
         'Ordinateur Portable',
         'Ordinateur portable haute performance',
         1200.00,
         50,
-        'C1',
-        'F1'
+        1,
+        1
     ),
     (
-        'P5',
         'Livre de Programmation',
         'Livre pour apprendre à programmer',
         35.00,
         150,
-        'C2',
-        'F2'
+        2,
+        2
     ),
     (
-        'P6',
         'T-shirt',
         'T-shirt en coton',
         20.00,
         200,
-        'C3',
-        'F3'
+        3,
+        3
     );
+    
+insert into Commandes (quantite, date_commande, id_produit)
+values 
+	(20, now(), 1),
+	(10, date_add(now(), interval 3 day), 2),
+	(30, date_add(now(), interval 5 day), 3);
 
 #Mise à jour d'un produit d'id = P1
+/*
 UPDATE Produits
 SET
     prix = 1300.00,
@@ -149,3 +143,4 @@ WHERE
 
 #Supprimez une commande spécifique
 DELETE FROM Commandes WHERE id_commande = 'C1';
+*/
